bd738a50c87b6228e1a898f7b99adb4a1c2ad7ed
Tomislav Hlupic <tomislav.hlupic@gmail.com>
2016-08-08 12:25:55 +0200

    Moved window loading

--- RpiLaserHostWpf/MainWindow.xaml.cs
+++ RpiLaserHostWpf/MainWindow.xaml.cs
@@ -22,11 +22,49 @@ namespace RpiLaserHostWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        // provjeriti dal ovo radi
         public MainWindow()
         {
             InitializeComponent();
 
             this.DataContext = new MainWindowTextViewModel();
+            
+            this.Loaded += (s,e) => OnLoaded();
+        }
+        
+        private void OnLoaded()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            var ipSelected = string.Empty;
+
+            foreach (var ip in host.AddressList)
+            {
+                var ipAddress = ip.ToString().Split('.');
+
+                if (ipAddress[0].Equals("192") && ipAddress[1].Equals("168") && ipAddress[2].Equals("1"))
+                {
+                    ipSelected = ip.ToString();
+                }
+            }
+
+            var baseAddress = $"http://localhost:9000/";
+
+            if (!string.IsNullOrWhiteSpace(ipSelected))
+            {
+                baseAddress = $"http://{ipSelected}:9000/";
+            }
+
+            // cijeli using u Task.Run
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                var console = MainWindow.DataContext as String; 
+                
+                // ovdje treba staviti while(true) {thread.sleep(100);}
+                console+= ($"Web Server is running on: {baseAddress}");
+            }
+        }
         }
     }
 }
